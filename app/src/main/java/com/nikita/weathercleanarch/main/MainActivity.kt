55c21ad2e6919215ebe995c116086ab3a24a91c5
nkarpunin <nikita.karpunin@iss.digital>
2021-12-13 20:12:26 +0300

    Показ главной активности

--- app/src/main/java/com/nikita/weathercleanarch/main/MainActivity.kt
+++ app/src/main/java/com/nikita/weathercleanarch/main/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.nikita.weathercleanarch.main
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.nikita.weathercleanarch.R
 
 class MainActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
